24adf48a0d0c66f769cf1dbdac8aec3a646ef381
baag6412 <baag6412@gmail.com>
2025-12-07 12:26:08 +0800

    finished

---
+++ script.js
@@ -0,0 +1,139 @@
+import { addIcon, editIcon } from "./icons.js";
+import { getTaskStructure } from "./structures.js";
+
+const input = document.querySelector("input");
+const form = document.querySelector("form");
+const taskListContainer = document.querySelector("#task-list");
+const taskSummary = document.getElementById("task-Summary");
+const progress = document.getElementById("progress");
+const submitTaskButton = document.querySelector("#submit-task");
+const microphone = document.getElementById("microphone");
+let tasks = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+    if (storedTasks) {
+        tasks = storedTasks;
+        updateTaskList();
+        updateStats();
+    }
+
+    const recognition = setUpSpeechRecognition();
+
+    microphone.addEventListener("click", () => {
+        recognition.start();
+        input.classList.add("active");
+    });
+
+    recognition.addEventListener("end", () => {
+        recognition.stop();
+        input.classList.remove("active");
+    });
+
+    recognition.addEventListener("result", (e) => {
+        const speechToText = e.results[0][0].transcript;
+        input.value = speechToText;
+    });
+
+    form.addEventListener("submit", submitForm);
+});
+
+const submitForm = (e) => {
+    e.preventDefault();
+
+    const text = input.value.trim();
+
+    if (!text) return;
+
+    const editingTask = tasks.find((task) => task.editing);
+
+    if (editingTask) {
+        editingTask.text = false;
+        submitTaskButton.innerHTML = addIcon;
+    } else {
+        tasks.push({ text, completed: false, editing: false });
+    }
+
+    form.reset();
+    updateTaskList();
+    updateStats();
+    saveTasks();
+};
+
+const updateTaskList = () => {
+    taskListContainer.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+
+        const completedClass = task.completed ? "completed" : "";
+        const editingClass = task.editing ? "completed" : "";
+
+        li.className = `task ${completedClass} ${editingClass}`;
+        li.innerHTML = getTaskStructure(task, index);
+
+        const checkbox = li.querySelector(".checkbox");
+        checkbox.addEventListener("change", () => toggleTaskCompleted(index));
+
+        const editButton = li.querySelector(".edit");
+        editButton.addEventListener("click", () => editTask(index));
+
+        const deleteButton = li.querySelector("#delete");
+        deleteButton.addEventListener("click", () => deleteTask(index));
+        taskListContainer.appendChild(li);
+    });
+};
+
+const toggleTaskCompleted = (index) => {
+    const selectedTask = tasks[index];
+    selectedTask.completed = !selectedTask.completed;
+    updateTaskList();
+    updateStats();
+    saveTasks();
+};
+
+const editTask = (index) => {
+    tasks.forEach((task) => (task.editing = false));
+
+    const selectedTask = tasks[index];
+    selectedTask.editing = true;
+
+    input.value = selectedTask.text;
+    submitTaskButton.innerHTML = editIcon;
+
+    updateTaskList();
+};
+
+const deleteTask = (index) => {
+    tasks.splice(index, 1);
+    updateTaskList();
+    updateStats();
+    saveTasks();
+};
+const updateStats = () => {
+    const completedTasks = tasks.filter((task) => task.completed);
+    const totalCompletedTasks = completedTasks.length;
+    const totalTasks = tasks.length;
+
+    const completionPercentage = (totalCompletedTasks / totalTasks) * 100;
+    progress.style.width = `${completionPercentage}%`;
+    taskSummary.textContent = `${totalCompletedTasks} / ${totalTasks}`;
+};
+
+const saveTasks = () => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+const setUpSpeechRecognition = () => {
+    const setUpSpeechRecognition =
+        window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+
+    recognition.lang = "mn-MN";
+    recognition.interimResult = false;
+    recognition.maxAlternatives = 1;
+
+    return recognition;
+};
